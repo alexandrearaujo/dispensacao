e6e95f97073cacb8e6ab201d400f8dbbc6d4da9c
alexandrearaujo <alexandre.garaujo@outlook.com>
2014-11-25 09:59:34 -0300

    teste

---
+++ dispensacao/src/main/java/br/com/mv/dispensacao/web/DispensacaoController.java
@@ -0,0 +1,43 @@
+package br.com.mv.dispensacao.web;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.mv.dispensacao.model.PedidoDispensacao;
+
+@Controller
+@ControllerAdvice
+public class DispensacaoController {
+	
+	
+	@RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+	
+	@ExceptionHandler(Exception.class)
+    public ModelAndView handleException(HttpServletRequest req, Exception e) {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("timestamp", new Date());
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName("exception");
+        return mav;
+    }
+	
+	@RequestMapping(value="/dispensar", method = RequestMethod.GET)
+    public String dispensar(Model model) {
+		model.addAttribute("pedidoDispensacao", new PedidoDispensacao());
+        return "dispensacao";
+    }
+
+}
